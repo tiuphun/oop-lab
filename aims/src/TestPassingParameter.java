e05340c6640ee95a5563baaefc7be895091e5b2d
tiuphun <tieuphuongnguyen01631@gmail.com>
2023-05-28 16:09:46 +0700

    passing parameter topic finished

--- aims/src/TestPassingParameter.java
+++ aims/src/TestPassingParameter.java
@@ -6,7 +6,7 @@ public class TestPassingParameter {
         
         swap(jungleDVD, cinderellaDVD);
         System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
-        System.out.println("Cinderella DVD title:" + cinderellaDVD.getTitle());
+        System.out.println("Cinderella DVD title: " + cinderellaDVD.getTitle());
         
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
         System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
@@ -22,5 +22,17 @@ public class TestPassingParameter {
         String oldTitle = dvd.getTitle();
         dvd.setTitle(title);
         dvd = new DigitalVideoDisc(oldTitle);
+    }
+    // After the call of swap(jungleDVD, cinderellaDVD) why does the title of these two objects still remain? 
+    //-> Because the swap() method only swap the value of the two objects' reference, not the value of the objects themselves.
+    // After the call of changeTitle(jungleDVD, cinderellaDVD.getTitle()) why is the title of the JungleDVD changed?
+    //-> Because the changeTitle() method changes the value of the object's title, not the value of the object's reference.
+    // All of this is because Java is a pass-by-value language.
+
+    // This swap() method can correctly swap the two objects.
+    public static void updatedSwap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        String tmp = dvd1.getTitle();
+        dvd1.setTitle(dvd2.getTitle());
+        dvd2.setTitle(tmp);
     }
 }
