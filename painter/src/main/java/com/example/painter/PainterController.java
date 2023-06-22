aba96c1d2b9071909b27558f4b4e7b698398977d
tiuphun <tieuphuongnguyen01631@gmail.com>
2023-06-22 10:02:06 +0700

    Painter program completed

--- painter/src/main/java/com/example/painter/PainterController.java
+++ painter/src/main/java/com/example/painter/PainterController.java
@@ -2,15 +2,23 @@ package com.example.painter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
 public class PainterController {
+    @FXML private Button clearButton;
+    @FXML private Pane drawingAreaPane;
+    @FXML private RadioButton eraserButton;
+    @FXML private RadioButton penButton;
+    @FXML private ToggleGroup toolsToggleGroup;
 
-    @FXML
-    private BorderPane drawingAreaPane;
+    private Paint brushColor = Color.BLACK;
 
     @FXML
     void clearButtonPressed(ActionEvent event) {
@@ -19,10 +27,22 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.DARKSLATEBLUE);
-        drawingAreaPane.getChildren().add(newCircle);
+        if (drawingAreaPane.getBoundsInParent().contains(event.getX(), event.getY())) {
+            Circle newCircle = new Circle(event.getX(), event.getY(), 4, brushColor);
+            drawingAreaPane.getChildren().add(newCircle);
+        }
     }
 
+    @FXML
+    void toolsButtonSelected(ActionEvent event) {
+        brushColor = (Color) toolsToggleGroup.getSelectedToggle().getUserData();
+    }
+
+    public void initialize () {
+        penButton.setUserData(Color.BLACK);
+        eraserButton.setUserData(Color.WHITE);
+    }
 }
+
 
 
